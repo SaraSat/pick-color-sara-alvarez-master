8811bd94ae73e36bca3055a1dadad3ca10facc1d
SaraSat <sara_sat93@hotmail.com>
2019-09-30 09:20:41 +0200

    Primero

---
+++ js/rgb.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Función que al pasar un valor RGB devuelva el resultado en representación hexadecimal
+*/
+function rgb(r,g,b) { 
+  return "#000000";
+}
+
+/* Función que al pasar un valor RGB en representación hexadecimal
+   devuelve la representación en formato decimal con el formato RRRGGGBB
+*/
+function hex(h) {
+   return "000000000";
+}
+
