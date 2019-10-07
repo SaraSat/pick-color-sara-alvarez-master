541b923d4d7c66d32b2837d5a33efa5b5446e9a6
Dannerino <55684477+Dannerino@users.noreply.github.com>
2019-10-07 09:46:17 +0200

    htmlMejorado

---
+++ rgb.js
@@ -0,0 +1,76 @@
+function rgb(r, g, b) {
+    r = parseInt(r);
+    g = parseInt(g);
+    b = parseInt(b);
+    rh = (r.toString(16)).toUpperCase();
+    gh = g.toString(16).toUpperCase();
+    bh = b.toString(16).toUpperCase();
+    if (r > 255) {
+        rh = "FF";
+    }
+    if (g > 255) {
+        gh = "FF";
+    }
+    if (b > 255) {
+        bh = "FF";
+    }
+    if (r < 0) {
+        rh = "00";
+    }
+    if (g < 0) {
+        gh = "00";
+    }
+    if (b < 0) {
+        bh = "00";
+    }
+
+    rh = rh.padStart(2, "0");
+    gh = gh.padStart(2, "0");
+    bh = bh.padStart(2, "0");
+
+    return "#" + rh + "" + gh + "" + bh;
+}
+
+
+
+
+
+
+
+document.getElementById("enviar2").addEventListener("click", function (e) {
+    var h = document.getElementById("hex").value;
+    var hexad = hex(h);
+    document.getElementById("r").value = hexad[0];
+    document.getElementById("g").value = hexad[1];
+    document.getElementById("b").value = hexad[2];
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+function hex(h) {
+    h = h.toString().toUpperCase();
+    var p = h.charAt(0);
+    var r = h.charAt(1) + h.charAt(2);
+    var g = h.charAt(3) + h.charAt(4);
+    var b = h.charAt(5) + h.charAt(6);
+    r = parseInt(r, 16).toString();
+    g = parseInt(g, 16).toString();
+    b = parseInt(b, 16).toString();
+    r = r.padStart(3, "0");
+    g = g.padStart(3, "0");
+    b = b.padStart(3, "0");
+    var hexad = [r, g, b];
+    return hexad;
+}
+
